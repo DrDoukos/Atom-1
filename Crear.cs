2609c7b18452e3eebb694f5705ec84faf7e35ff3
Hafnio <luparia.ariel@gmail.com>
2017-06-28 13:47:54 -0300

    Create Crear.cs

---
+++ Crear.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class Crear : MonoBehaviour
+{
+    public int ID;
+
+	void Start ()
+    {
+	
+	}
+
+	void Update ()
+    {
+	
+	}
+
+    private int IDs()
+    {
+        return ID;
+    }
+}
